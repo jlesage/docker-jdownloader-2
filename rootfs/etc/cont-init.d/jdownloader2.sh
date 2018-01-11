82702ad86cef5657624976b60d172464333b2e74
Jocelyn Le Sage <jocelyn@le-sage.com>
2018-01-10 19:35:18 -0500

    Do not fail when taking ownership of /output fails but user has write access.

--- rootfs/etc/cont-init.d/jdownloader2.sh
+++ rootfs/etc/cont-init.d/jdownloader2.sh
@@ -3,6 +3,10 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
+log() {
+    echo "[cont-init.d] $(basename $0): $*"
+}
+
 # Make sure mandatory directories exist.
 mkdir -p /config/logs
 
@@ -14,6 +18,14 @@ fi
 chown -R $USER_ID:$GROUP_ID /config
 
 # Take ownership of the output directory.
-chown $USER_ID:$GROUP_ID /output
+if ! chown $USER_ID:$GROUP_ID /output; then
+    # Failed to take ownership of /output.  This could happen when,
+    # for example, the folder is mapped to a network share.
+    # Continue if we have write permission, else fail.
+    if s6-setuidgid $USER_ID:$GROUP_ID [ ! -w /output ]; then
+        log "ERROR: Failed to take ownership and no write permission on /output."
+        exit 1
+    fi
+fi
 
 # vim: set ft=sh :
